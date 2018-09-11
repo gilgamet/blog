a0076f43bafd1d02a95cf7b1e71ad139573a66f7
gilgamet <gilgamet@free.fr>
2018-09-11 10:54:02 +0200

    modif de src dans index pour tinymce

--- app/Views/template/default.php
+++ app/Views/template/default.php
@@ -43,9 +43,8 @@
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
     <!--<script src="../../assets/js/vendor/popper.min.js"></script>
     <script src="../../dist/js/bootstrap.min.js"></script>-->
-    <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
-    <script type='text/javascript' src='public/js/plugins/tinymce.min.js'></script>
-    <script type="text/javascript" src="public/js/plugins/init_tinymce.js"></script>
+    <script type='text/javascript' src='../public/js/plugins/tinymce.min.js'></script>
+    <script type="text/javascript" src="../public/js/plugins/init_tinymce.js"></script>
   </body>
 </html>
 
